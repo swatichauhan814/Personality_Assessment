1eb6d7b0cab684bc41d22f19d2eec50859fe3d3b
Swati Chauhan <swati.chauhan814@gmail.com>
2020-05-13 10:32:26 +0530

    updated folder name

--- main/settings.py
+++ main/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'minor.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'minor.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
